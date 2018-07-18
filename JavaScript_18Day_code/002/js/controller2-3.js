830a7eb6b7a4b436e2b73d60399d200b3852bbb1
YuanYuan-Cai <iscaiyy@foxmail.com>
2018-07-18 23:46:21 +0800

    '002_Add'

---
+++ JavaScript_18Day_code/002/js/controller2-3.js
@@ -0,0 +1,26 @@
+//document.write('<div>Hello2 World!</div>')
+
+//声明一个变量
+//var x;
+////给变量赋值，立即数
+//x = 1;
+
+//声明时同时赋值
+var x = 1;
+
+//给变量赋值，其他变量
+var y = x;
+
+y = x + 1 + 11 -6;
+
+//声明多个变量
+var a1,a2,a3;
+
+var b1 = 4,
+    b2 = 5,
+    b3 = 6;
+    
+document.write('b1 = ' + b1 + '<br>');
+document.write('b2 = ' + b2 + '<br>');
+document.write('b3 = ' + b3 + '<br>');
+
