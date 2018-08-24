5a3dfef976c5803d8e173100529282d47bb05957
YuanYuan-Cai <iscaiyy@foxmail.com>
2018-08-24 18:56:28 +0800

    005类型转换&数组：typeof更新

---
+++ JavaScript_18Day_code/005/js/controller5-1.js
@@ -0,0 +1,11 @@
+//类型转换&数组
+
+//为什么要用console.log
+var obj={};
+document.write(obj);
+console.log(obj);
+
+//typeof
+var x = 1;
+console.log('[typeof] x ' + typeof(x));
+console.log('[typeof] x ' + typeof x);
