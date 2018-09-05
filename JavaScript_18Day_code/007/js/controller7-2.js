212d0189a78d178f41ca6a70937bf1f4f935f2fa
YuanYuan-Cai <iscaiyy@foxmail.com>
2018-09-05 23:31:07 +0800

    007天：【课时20】object下

--- JavaScript_18Day_code/007/js/controller7-2.js
+++ JavaScript_18Day_code/007/js/controller7-2.js
@@ -14,3 +14,44 @@ obj.b = 'bbb';
 obj.f1 = function(){
 	console.log('ff.........');
 };
+
+//function f2(){
+//	aa = 5;//实际上等价于window.aa=5
+////	var aa = 5;
+//};
+//f2();
+//alert(aa);
+
+
+//查询一个属性是否存在？
+//1.in
+//console.log(('a' in obj));
+////2.hasOwnProperty()
+//console.log(obj.hasOwnProperty('a'));
+//
+////删除一个属性
+//// delete
+//delete obj.a;
+//console.log(('a' in obj));
+//console.log(obj.hasOwnProperty('a'));
+
+//var arr = [];
+//arr['a'] = 'c';
+//delete arr['a'];
+//console.log(arr);
+
+// 修改一个属性
+obj.a = 123;
+
+// 枚举
+// 其自己属性可以枚举
+// 继承来的不能枚举出
+for(var p in obj){
+	console.log(p);
+}
+
+obj.toString();//没有枚举出来
+
+//判断对象的属性是否能枚举：
+console.log(obj.propertyIsEnumerable('a'));
+console.log(obj.propertyIsEnumerable('toString()'));
