1f7ad1e84baeb3f4169a4f507e2b9e7aa03813f5
YuanYuan-Cai <iscaiyy@foxmail.com>
2018-09-21 18:54:40 +0800

    009天+010天：【课时26】类数组（下）+【课时27】String（上）

--- JavaScript_18Day_code/009/js/controller9-5.js
+++ JavaScript_18Day_code/009/js/controller9-5.js
@@ -1,13 +1,73 @@
 //类数组
-var arr = [1,2,3];
+//1，有下标，有length，有push和splice，就是一个类数组
+//2，既可以当数组用，也可以当对象用
+//3，DOM里面的数组都是类数组
+//var arr = [1,2,3];
+//
+//var obj = {
+//	'0':1,
+//	'1':2,
+//	'2':3,
+//	'length':3,
+//	'push':Array.prototype.push,
+//	'splice':Array.prototype.splice
+//};
+//
+//arr[3] = 4;
+//obj['3'] = 4;
+//
+//arr.push(4);
+//obj.push(4);
+//-------
+//var obj = {
+//	'2':'a',
+//	'3':'b',
+//	'length':2
+//	'push':Array.prototype.push,
+//	'splice':Array.prototype.splice
+//}
 
-var obj = {
-	'0':1,
-	'1':2,
-	'2':3,
-	'length':3,
-	'push':Array.prototype.push,
-	'splice':Array.prototype.splice
-};
+//obj.push('c');
+//obj.push('d');
+//↑运行出↓
+//var obj = {
+//	'2':'c',
+//	'3':'d',
+//	'length':2
+//	'push':Array.prototype.push,
+//	'splice':Array.prototype.splice
+//}
+//原因↓是push的实现方法的原因
+//obj.push = function(x){
+//	obj[obj.length] = x;
+//	obj.length++;
+//}
 
-//【课时26】类数组 32:31
+//闭包练习
+function fa(x){
+	var aaa = 0;
+	function fb(x){
+		if(x > 0){
+			return fb(x-1);
+		}else{
+			aaa++;
+			return aaa;
+		}
+	}
+	
+	if(x > 0){
+		return fa(x-1);
+	}else{
+		return fb;
+	}
+}
+
+var fb1 = fa(0);
+console.log(fb1());
+
+var fb2 = fa(1);
+console.log(fb2());
+console.log(fb1());
+console.log(fb1());
+console.log(fb1());
+console.log(fb2());
---
+++ JavaScript_18Day_code/010/js/controller10-1.js
@@ -0,0 +1,35 @@
+//String对象
+//	- 构造函数
+//	- 成员函数
+//字符集
+//	- Unicode系列
+//	- GBK系列
+//JSON
+
+// String()和string
+
+var str = String(12345);
+
+//console.log(typeof(str));
+str.a = 5;
+// var tmp = new String(str);
+// tmp.a = 5;
+// tmp摧毁
+//console.log(str.a);
+// var tmp1 = new String(str);
+// 访问tmp1.a;
+// tmp1摧毁
+
+var objString = new String(12345);
+//console.log(objString);
+//objString.a = 5;
+//console.log(objString.a);
+
+// String可以用数组方式访问
+//for(var i = 0;i < objString.length;i++){
+//	console.log(objString[i]);
+//}
+//
+//for(var i = 0;i < str.length;i++){
+//	console.log(str[i]);//包装类
+//}
