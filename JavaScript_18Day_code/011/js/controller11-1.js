b76a68692be640e0554fc9e77cd3a652fb4262c3
YuanYuan-Cai <iscaiyy@foxmail.com>
2018-09-27 00:18:48 +0800

    【课时31】JSON（下）

--- JavaScript_18Day_code/011/js/controller11-1.js
+++ JavaScript_18Day_code/011/js/controller11-1.js
@@ -1,7 +1,7 @@
 // Date对象
 // 1，如何获得当前时间
 
-//var d1 = new Date();
+var d1 = new Date();
 //console.log(d1);
 //
 //// 2,数：1970年1月1日到现在的毫秒数
@@ -11,7 +11,42 @@
 //x = x / (1000 * 3600 * 24 * 365);
 //console.log(x);
 
-// 3, setFullYear,
-// 【课时31】JSON 55:02
+// 3, setFullYear, 月份设定 0~11，其他不变
+//d1.setFullYear(2017,4,1);
+//console.log(d1);
 
+// 4,有参数的构造函数
+//var d2 = new Date(2017,4,1);
+//console.log(d2);
 
+// 5,GMT 格林尼治时间；UTC 协调世界时间
+//console.log(d1.toUTCString());
+//console.log(d1.toGMTString());
+
+// 6，设定年setFullYear，getFullYear
+//d1.setFullYear(2015);
+//console.log(d1);
+//d1.setFullYear(d1.getFullYear() + 1);
+//console.log(d1);
+
+// 7,设定月 0~11(记得加1)
+console.log(d1.getMonth() + 1);
+
+// 8，设定日（是几就是几）
+console.log(d1.getDate());
+
+// 9，设定星期几 0：星期日，1~6：周一~周六
+console.log(d1.getDay());
+
+// 10,获得毫秒数
+var haomiaoshu = Date.parse(d1.toString());
+console.log(haomiaoshu);
+
+// 11，Date 克隆
+var d4 = new Date(d1);
+console.log(d4);
+
+// 12,比较
+var d5 = new Date(2018,0,1);
+var d6 = new Date(2017,0,1);
+console.log((d5 - d6)/(1000 * 3600 * 24));
