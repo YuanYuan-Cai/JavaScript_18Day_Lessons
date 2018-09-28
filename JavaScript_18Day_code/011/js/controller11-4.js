7df32697237ef02daf8608ac49f11327874a41a1
YuanYuan-Cai <iscaiyy@foxmail.com>
2018-09-28 18:49:37 +0800

    【课时34】Math对象+【课时35】实例操作

--- JavaScript_18Day_code/011/js/controller11-4.js
+++ JavaScript_18Day_code/011/js/controller11-4.js
@@ -1,5 +1,54 @@
 // Math对象
 
-console.log(Math.E);
+// 常数
+//console.log(Math.E);
+//console.log(Math.LN2);
+//console.log(Math.LN10);
+//console.log(Math.LOG2E);
+//console.log(Math.LOG10E);
+//console.log(Math.PI);
+//console.log(Math.SQRT1_2);
+//console.log(Math.SQRT2);
+//
+//// 方法
+//// 绝对值
+//console.log(Math.abs(-5));
+//
+//// 三角函数
+//console.log(Math.sin(Math.PI/3));
+//console.log(Math.cos(Math.PI/3));
+//console.log(Math.tan(Math.PI/3));
+//
+////反三角函数
+//console.log(Math.asin(1));
+//console.log(Math.acos(1));
+//console.log(Math.atan(1));
+//console.log(Math.atan2((1,1)));//(x,y)
+//
+//// 四舍五入，以及取整
+//console.log(Math.round(13.4));
+//console.log(Math.round(13.5));
+//console.log(Math.floor(13.5));//向下取整
+//console.log(Math.ceil(13.2));//向上取整
 
-【课时34】Math对象
+// 指数和对数
+console.log(Math.exp(2));
+console.log(Math.log(25));//对e取对数
+
+console.log(Math.pow(2,2));
+console.log(Math.sqrt(2));
+
+// 最大值与最小值
+console.log(Math.max(1,2444,55,2));
+console.log(Math.min(1,2444,55,2));
+
+// 随机数
+console.log(Math.random());//0~1之间的均匀分布
+console.log(Math.random());
+console.log(Math.random());
+
+// Math Object
+console.log(Math);
+console.log(Math.valueOf());
+
+Math.myValue = 0.5;
